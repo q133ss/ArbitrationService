154fd85e5266d8c87fa3d4010761f9322db9b90d
q133ss <info@promo-kick.ru>
2023-10-22 05:48:05 +0300

    admin chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        if(Auth()->user()->role->tech_name == 'admin'){
+            $leadsQuery = DB::table('leads')
+                ->select(DB::raw('DATE(created_at) as date'), DB::raw('COUNT(*) as count'))
+                ->where('created_at', '>=', now()->subWeek())
+                ->groupBy('date')
+                ->get()
+                ->toArray();
+
+            $leads = [];
+
+            $sums = [];
+
+            $sumsQuery = DB::table('leads')
+                ->where('leads.created_at', '>=', now()->subWeek())
+                ->leftJoin('offers', 'offers.id', 'leads.offer_id')
+                ->select(DB::raw('DATE(leads.created_at) as date'), DB::raw('SUM(offers.price) as total_price'))
+                ->groupBy('leads.created_at')
+                ->get()
+                ->toArray();
+
+            foreach ($sumsQuery as $sum){
+                $sums[$sum->date] = $sum->total_price;
+            }
+
+            foreach ($leadsQuery as $lead) {
+                $leads[$lead->date] = $lead->count;
+            }
+
+            // Заполняем отсутствующие даты значением 9
+            $startDate = now()->subWeek()->startOfDay();
+            $endDate = now()->endOfDay();
+            $currentDate = $startDate;
+            while ($currentDate <= $endDate) {
+                $dateString = $currentDate->toDateString();
+
+                if (!isset($leads[$dateString])) {
+                    $leads[$dateString] = 0;
+                }
+
+                if (!isset($sums[$dateString])) {
+                    $sums[$dateString] = 0;
+                }
+
+                $currentDate->addDay();
+            }
+
+            ksort($leads);
+            ksort($sums);
+
+            return view(Auth()->user()->role->tech_name.'.index', compact('leads', 'sums'));
+        }
         return view(Auth()->user()->role->tech_name.'.index');
     }
 }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,170 @@
 @extends('layouts.dashboard')
 @section('title', 'Главная')
 @section('content')
-    Admin
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Лиды за неделю </h5>
+                </div>
+                <div class="card-body">
+                    <div id="basic-apex"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Сумма лидов за неделю </h5>
+                </div>
+                <div class="card-body">
+                    <div id="sum"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('scripts')
+    <script>
+        var options = {
+            series: [{
+                name: "Колличество лидов",
+                data: [
+                    @foreach($leads as $lead)
+                    {{$lead}},
+                    @endforeach
+                ]
+            }],
+            chart: {
+                type: 'area',
+                height: 350,
+                defaultLocale: 'ru',
+                locales: [{
+                    name: 'ru',
+                    options: {
+                        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                        shortMonths: ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+                        days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+                        shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                        toolbar: {
+                            download: 'Скачать SVG',
+                            selection: 'Выбор',
+                            selectionZoom: 'Масштаб выбора',
+                            zoomIn: 'Приблизить',
+                            zoomOut: 'Отдалить',
+                            pan: 'Панорамирование',
+                            reset: 'Убрать zoom',
+                        }
+                    }
+                }],
+                zoom: {
+                    enabled: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'straight'
+            },
+
+            title: {
+                text: 'Колличество лидов за каждый день',
+                align: 'left'
+            },
+            subtitle: {
+                text: '',
+                align: 'left'
+            },
+            labels: [
+                @foreach($leads as $date => $count)
+                    '{{$date}}',
+                @endforeach
+            ],
+            xaxis: {
+                type: 'datetime',
+            },
+            yaxis: {
+                opposite: true
+            },
+            legend: {
+                horizontalAlign: 'left'
+            }
+        };
+
+        // SuM
+
+        var optionsSum = {
+            series: [{
+                name: "Сумма лидов",
+                data: [
+                    @foreach($sums as $sum)
+                        '{{$sum}}',
+                    @endforeach
+                ]
+            }],
+            chart: {
+                type: 'area',
+                height: 350,
+                defaultLocale: 'ru',
+                locales: [{
+                    name: 'ru',
+                    options: {
+                        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                        shortMonths: ['Янв', 'Фев', 'Март', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+                        days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+                        shortDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+                        toolbar: {
+                            download: 'Скачать SVG',
+                            selection: 'Выбор',
+                            selectionZoom: 'Масштаб выбора',
+                            zoomIn: 'Приблизить',
+                            zoomOut: 'Отдалить',
+                            pan: 'Панорамирование',
+                            reset: 'Убрать zoom',
+                        }
+                    }
+                }],
+                zoom: {
+                    enabled: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'straight'
+            },
+
+            title: {
+                text: 'Колличество лидов за каждый день',
+                align: 'left'
+            },
+            subtitle: {
+                text: '',
+                align: 'left'
+            },
+            labels: [
+                @foreach($leads as $date => $lead)
+                    '{{$date}}',
+                @endforeach
+            ],
+            xaxis: {
+                type: 'datetime',
+            },
+            yaxis: {
+                opposite: true
+            },
+            legend: {
+                horizontalAlign: 'left'
+            }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#basic-apex"), options);
+        chart.render();
+
+        var sumChart = new ApexCharts(document.querySelector("#sum"), optionsSum);
+        sumChart.render();
+    </script>
 @endsection
